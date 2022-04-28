92da4ce1fc72273539ae5960dce4db4844bfaac7
Mohammed <nodymoha@yahoo.com>
2022-04-27 21:50:30 -0400

    Athan Umm-Ulqura (Makkah)

---
+++ athan.py
@@ -0,0 +1,58 @@
+import time
+from pygame import mixer
+import string
+import praytimes
+from datetime import date
+athan_sound = 'makkah.mp3'
+
+while (1):
+
+        tmm = praytimes.PrayTimes().getTimes(date.today(),[30.45,-84.25],-5)
+        FAJR=tmm['fajr']
+        DHUHR=tmm['dhuhr']
+        ASR=tmm['asr']
+        MAGHRIB=tmm['maghrib']
+        ISHA=tmm['isha']
+
+	
+        tempT= time.strftime(str('%H'))
+        currTime= tempT
+        tempT= time.strftime(str('%M'))
+        currTime= currTime +':'+ tempT
+        if currTime == FAJR:
+                print('Fajr athan')
+                mixer.init(16000)
+                mixer.music.load(athan_sound)
+                mixer.music.play()
+                while mixer.music.get_busy() == True:
+                        continue
+        if currTime == DHUHR:
+                print('Dhuhr athan')
+                mixer.init(16000)
+                mixer.music.load(athan_sound)
+                mixer.music.play()
+                while mixer.music.get_busy() == True:
+                        continue
+        if currTime == ASR:
+                print('Asr athan')
+                mixer.init(16000)
+                mixer.music.load(athan_sound)
+                mixer.music.play()
+                while mixer.music.get_busy() == True:
+                        continue
+        if currTime == MAGHRIB:
+                print('Maghrib athan')
+                mixer.init(16000)
+                mixer.music.load(athan_sound)
+                mixer.music.play()
+                while mixer.music.get_busy() == True:
+                        continue
+        if currTime == ISHA:
+                print('Isha athan')
+                mixer.init(16000)
+                mixer.music.load(athan_sound)
+                mixer.music.play()
+                while mixer.music.get_busy() == True:
+                        continue
+        time.sleep(1)
+        
